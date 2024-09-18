99aaa87517eed0744d24e5f709fff43721558d4e
Chuck McCallum <mccallucc@gmail.com>
2024-09-18 18:58:17 -0400

    switch to core api since that will make the programatic interface easier

--- dp_creator_ii/app.py
+++ dp_creator_ii/app.py
@@ -1,31 +1,16 @@
-from shiny import App, reactive, render, ui
-from datetime import datetime
+from shiny import App, ui
 
-app_ui = ui.page_fixed(
-    ui.h1("Title"),
-    ui.output_code("greeting"),
+app_ui = ui.page_fluid(
+    ui.navset_tab(  
+        ui.nav_panel("Dataset", "TODO: Pick dataset"),
+        ui.nav_panel("Analysis", "TODO: Define analysis"),
+        ui.nav_panel("Download", "TODO: Download results"),
+    )  
 )
 
-def server(input, output, session):
-    @reactive.calc
-    def time():
-        reactive.invalidate_later(1)
-        return datetime.now()
 
-    @render.code
-    def greeting():
-        return f"Hello, world!\nIt's currently {time()}."
+def server(input, output, session):
+    pass
+
 
 app = App(app_ui, server)
-
-# from shiny.express import ui
-
-# with ui.navset_pill(id="tab"):  
-#     with ui.nav_panel("Dataset"):
-#         "TODO: Pick dataset"
-
-#     with ui.nav_panel("Analysis"):
-#         "TODO: Define analysis"
-
-#     with ui.nav_panel("Download"):
-#         "TODO: Download results"
