5323e43633e44799787c8dddd742556392e887e8
Chuck McCallum <mccallucc@gmail.com>
2024-09-18 19:02:10 -0400

    and open browser

--- dp_creator_ii/__init__.py
+++ dp_creator_ii/__init__.py
@@ -7,4 +7,4 @@ def main():
     import os
     from pathlib import Path
     os.chdir(Path(__file__).parent) # run_app() depends on the CWD.
-    shiny.run_app()
+    shiny.run_app(launch_browser=True)
