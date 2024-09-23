ad6ae50326fa4d2ecb0af54f98a0358a596e21b0
Chuck McCallum <mccallucc@gmail.com>
2024-09-23 18:24:02 -0400

    try running the generated code

--- dp_creator_ii/tests/test_template.py
+++ dp_creator_ii/tests/test_template.py
@@ -1,12 +1,16 @@
 from dp_creator_ii.template import Template
+import opendp.prelude as dp
 
 
 def test_template():
     context_template = Template('context.py')
     context_code = context_template.fill({
-        'CSV_PATH': '"fake.csv"',
+        'CSV_PATH': '"dp_creator_ii/tests/fake.csv"',
         'UNIT': '1',
         'LOSS': '1',
         'WEIGHTS': '[1]'
     })
-    assert 'data=pl.scan_csv("fake.csv")' in context_code
+
+    globals = {}
+    exec(context_code, globals)
+    assert isinstance(globals['context'], dp.Context)
