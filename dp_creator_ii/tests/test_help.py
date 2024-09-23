42439c5c43cc6dd1b86da4ff1c758c78fc5ee371
Chuck McCallum <mccallucc@gmail.com>
2024-09-23 15:21:59 -0400

    actually update variable

--- dp_creator_ii/tests/test_help.py
+++ dp_creator_ii/tests/test_help.py
@@ -5,7 +5,7 @@ import dp_creator_ii
 
 def test_help():
     help = dp_creator_ii.get_parser().format_help()
-    help.replace('optional arguments:', 'options:')  # Python 3.9 is different.
+    help = help.replace('optional arguments:', 'options:')  # Python 3.9 is different.
     print(help)
 
     readme_md = (Path(__file__).parent.parent.parent / 'README.md').read_text()
