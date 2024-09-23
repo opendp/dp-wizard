db4e7f211f96373ab34b6088f69a1a2227a9996b
Chuck McCallum <mccallucc@gmail.com>
2024-09-23 15:24:35 -0400

    linting

--- dp_creator_ii/tests/test_help.py
+++ dp_creator_ii/tests/test_help.py
@@ -5,7 +5,8 @@ import dp_creator_ii
 
 def test_help():
     help = dp_creator_ii.get_parser().format_help()
-    help = help.replace('optional arguments:', 'options:')  # Python 3.9 is different.
+    # Text is a little different under Python 3.9:
+    help = help.replace('optional arguments:', 'options:')
     print(help)
 
     readme_md = (Path(__file__).parent.parent.parent / 'README.md').read_text()
