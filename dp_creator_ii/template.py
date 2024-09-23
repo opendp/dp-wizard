deb22e5044a8b217f297c2295ce8ae2c0b8795b1
Chuck McCallum <mccallucc@gmail.com>
2024-09-23 17:51:52 -0400

    ignore undefined names

--- dp_creator_ii/template.py
+++ dp_creator_ii/template.py
@@ -5,7 +5,7 @@ class Template:
     def __init__(self, path):
         template_path = Path(__file__).parent / 'templates' / path
         self.template = template_path.read_text()
-    
+
     def fill(self, map):
         filled = self.template
         for k, v in map.items():
