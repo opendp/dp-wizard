eb6a786ec1e77ef2565aa1a34fcdace52cb58873
Chuck McCallum <mccallucc@gmail.com>
2024-09-25 13:58:56 -0400

    buttons to navigate between panels

--- dp_creator_ii/app.py
+++ dp_creator_ii/app.py
@@ -7,18 +7,30 @@ config_path = Path(__file__).parent / "config.json"
 config = json.loads(config_path.read_text())
 config_path.unlink()
 
-app_ui = ui.page_fluid(
-    ui.output_text("value"),
+app_ui = ui.page_bootstrap(
     ui.navset_tab(
         ui.nav_panel(
             "Select Dataset",
             "TODO: Pick dataset",
             ui.output_text("csv_path_text"),
             ui.output_text("unit_of_privacy_text"),
+            ui.input_action_button("go_to_analysis", "Perform analysis"),
+            value="dataset_panel",
         ),
-        ui.nav_panel("Perform Analysis", "TODO: Define analysis"),
-        ui.nav_panel("Download Results", "TODO: Download results"),
+        ui.nav_panel(
+            "Perform Analysis",
+            "TODO: Define analysis",
+            ui.input_action_button("go_to_results", "Download results"),
+            value="analysis_panel",
+        ),
+        ui.nav_panel(
+            "Download Results",
+            "TODO: Download results",
+            value="results_panel",
+        ),
+        id="top_level_nav",
     ),
+    title="DP Creator II",
 )
 
 
@@ -33,6 +45,16 @@ def server(input, output, session):
     @render.text
     def unit_of_privacy_text():
         return str(unit_of_privacy.get())
+
+    @reactive.effect
+    @reactive.event(input.go_to_analysis)
+    def go_to_analysis():
+        ui.update_navs("top_level_nav", selected="analysis_panel")
+
+    @reactive.effect
+    @reactive.event(input.go_to_results)
+    def go_to_results():
+        ui.update_navs("top_level_nav", selected="results_panel")
 
 
 app = App(app_ui, server)
