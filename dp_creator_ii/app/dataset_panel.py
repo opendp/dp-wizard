28e06f6312d57accdde61b19958a250443ecea3b
Chuck McCallum <mccallucc@gmail.com>
2024-09-30 16:13:36 -0400

    details-summary-pre

--- dp_creator_ii/app/dataset_panel.py
+++ dp_creator_ii/app/dataset_panel.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import json
 from shiny import ui, reactive, render
 from dp_creator_ii import get_arg_parser
+from htmltools.tags import details, pre, summary
 
 
 def dataset_ui():
@@ -10,6 +11,10 @@ def dataset_ui():
         "TODO: Pick dataset",
         ui.output_text("csv_path_text"),
         ui.output_text("unit_of_privacy_text"),
+        details(
+            summary("Example code"),
+            pre("privacy_unit = dp.unit_of(contributions=1)"),
+        ),
         ui.input_action_button("go_to_analysis", "Perform analysis"),
         value="dataset_panel",
     )
@@ -27,6 +32,11 @@ def dataset_server(input, output, session):
     @render.text
     def unit_of_privacy_text():
         return str(unit_of_privacy.get())
+
+    @render.text
+    def unit_of_privacy_python():
+        contributions = unit_of_privacy.get()
+        return f"privacy_unit = dp.unit_of(contributions={contributions})"
 
     @reactive.effect
     @reactive.event(input.go_to_analysis)
