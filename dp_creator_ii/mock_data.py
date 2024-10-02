a768ce9632e9f283fbf9455dfe4ea1d90a27847f
Chuck McCallum <mccallucc@gmail.com>
2024-10-02 17:21:05 -0400

    switch loops

--- dp_creator_ii/mock_data.py
+++ dp_creator_ii/mock_data.py
@@ -8,13 +8,14 @@ ColumnDef = namedtuple("ColumnDef", ["min", "max"])
 def mock_data(column_defs, row_count=1000):
     schema = {column_name: float for column_name in column_defs.keys()}
     data = {column_name: [] for column_name in column_defs.keys()}
-    for i in range(row_count + 1):
-        for column_name, column_def in column_defs.items():
-            quantile_width = 2 / 3
+
+    quantile_width = 2 / 3
+    for column_name, column_def in column_defs.items():
+        scale = column_def.max - column_def.min
+        center = (column_def.max + column_def.min) / 2
+        for i in range(row_count + 1):
             quantile = (quantile_width * i / row_count) + (1 - quantile_width) / 2
             ppf = norm.ppf(quantile)
-            scale = column_def.max - column_def.min
-            center = (column_def.max + column_def.min) / 2
             value = ppf * scale / 2 + center
             data[column_name].append(value)
     return pl.DataFrame(data=data, schema=schema)
