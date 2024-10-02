a3670ee69afe1e48f19ebeb4f04fd076400eb9ec
Chuck McCallum <mccallucc@gmail.com>
2024-10-02 17:18:31 -0400

    math is right

--- dp_creator_ii/mock_data.py
+++ dp_creator_ii/mock_data.py
@@ -8,9 +8,13 @@ ColumnDef = namedtuple("ColumnDef", ["min", "max"])
 def mock_data(column_defs, row_count=1000):
     schema = {column_name: float for column_name in column_defs.keys()}
     data = {column_name: [] for column_name in column_defs.keys()}
-    for i in range(row_count):
+    for i in range(row_count + 1):
         for column_name, column_def in column_defs.items():
-            quantile = i / row_count / 2 + 0.25  # ie, 25th to 75th percentiles
-            value = norm.ppf(quantile)
+            quantile_width = 2 / 3
+            quantile = (quantile_width * i / row_count) + (1 - quantile_width) / 2
+            ppf = norm.ppf(quantile)
+            scale = column_def.max - column_def.min
+            center = (column_def.max + column_def.min) / 2
+            value = ppf * scale / 2 + center
             data[column_name].append(value)
     return pl.DataFrame(data=data, schema=schema)
--- dp_creator_ii/tests/test_mock_data.py
+++ dp_creator_ii/tests/test_mock_data.py
@@ -4,6 +4,6 @@ from dp_creator_ii.mock_data import mock_data, ColumnDef
 def test_mock_data():
     col_0_100 = ColumnDef(0, 100)
     col_neg_pos = ColumnDef(-10, 10)
-    df = mock_data({"col_0_100": col_0_100, "col_neg_pos": col_neg_pos}, row_count=5)
+    df = mock_data({"col_0_100": col_0_100, "col_neg_pos": col_neg_pos})
     print(df)
     assert df == None
