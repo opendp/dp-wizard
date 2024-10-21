29c69f0fef42030848d1105f502f209a1dc65dbf
Chuck McCallum <mccallucc@gmail.com>
2024-10-21 11:26:43 -0400

    label log slider

--- dp_creator_ii/app/analysis_panel.py
+++ dp_creator_ii/app/analysis_panel.py
@@ -27,7 +27,15 @@ def analysis_ui():
             "Values above 1 will add less noise to the data, "
             "but have greater risk of revealing individual data."
         ),
-        ui.input_slider("log_epsilon_slider", None, -1, 1, 0, step=0.1),
+        ui.tags.table(
+            ui.tags.tr(
+                ui.tags.td("0.1"),
+                ui.tags.td(
+                    ui.input_slider("log_epsilon_slider", None, -1, 1, 0, step=0.1),
+                ),
+                ui.tags.td("10.0"),
+            ),
+        ),
         ui.output_text("epsilon"),
         ui.markdown(
             "## Preview\n"
