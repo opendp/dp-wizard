51eb745f8fa1ef7122dce8358ed310832084f66f
Chuck McCallum <mccallucc@gmail.com>
2024-10-24 09:27:56 -0400

    _df_to_dict doc test

--- dp_creator_ii/app/components/plots.py
+++ dp_creator_ii/app/components/plots.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 
 
-def _df_to_dict(df):  # pragma: no cover
-    # TODO: Add doctest
+def _df_to_dict(df):
+    """
+    >>> import polars as pl
+    >>> df = pl.DataFrame({
+    ...     "bin": ["A", "B", "C"],
+    ...     "len": [0, 10, 20],
+    ... })
+    >>> _df_to_dict(df)
+    {'A': 0, 'B': 10, 'C': 20}
+    """
     return {
         # The name of the key will vary, so just get the first value.
         list(range_len.values())[0]: range_len["len"]
@@ -17,3 +25,4 @@ def plot_histogram(histogram_df, error, cutoff):  # pragma: no cover
     bar_colors = ["blue" if v > cutoff else "lightblue" for v in values]
     axes.bar(labels, values, color=bar_colors, yerr=error)
     axes.axhline(cutoff, color="lightgrey", zorder=-1)
+    # TODO: Since this seems to return None, how does the information flow?
