58a051f91217c566b573ded394925fc870f65090
Chuck McCallum <mccallucc@gmail.com>
2024-10-25 08:52:35 -0400

    Move DP details out

--- dp_creator_ii/app/components/column_module.py
+++ dp_creator_ii/app/components/column_module.py
@@ -1,13 +1,9 @@
 from shiny import ui, render, module, reactive
-import opendp.prelude as dp
-import polars as pl
 
-from dp_creator_ii.utils.mock_data import mock_data, ColumnDef
+from dp_creator_ii.utils.dp_helper import make_confidence_accuracy_histogram
 from dp_creator_ii.app.components.plots import plot_histogram
 from dp_creator_ii.utils.templates import make_column_config_block
 from dp_creator_ii.app.components.outputs import output_code_sample
-
-dp.enable_features("contrib")
 
 
 @module.ui
@@ -34,24 +30,6 @@ def column_ui():  # pragma: no cover
         ),
         ui.output_plot("column_plot"),
     ]
-
-
-def _make_cut_points(lower, upper, bin_count):
-    """
-    Returns one more cut point than the bin_count.
-    (There are actually two more bins, extending to
-    -inf and +inf, but we'll ignore those.)
-    Cut points are evenly spaced from lower to upper.
-
-    >>> _make_cut_points(0, 10, 1)
-    [0.0, 10.0]
-    >>> _make_cut_points(0, 10, 2)
-    [0.0, 5.0, 10.0]
-    >>> _make_cut_points(0, 10, 3)
-    [0.0, 3.33, 6.67, 10.0]
-    """
-    bin_width = (upper - lower) / bin_count
-    return [round(lower + i * bin_width, 2) for i in range(bin_count + 1)]
 
 
 @module.server
@@ -87,7 +65,7 @@ def column_server(
         max_x = config["max"]
         bin_count = config["bins"]
         weight = config["weight"]
-        _confidence, accuracy, histogram = _make_confidence_accuracy_histogram(
+        _confidence, accuracy, histogram = make_confidence_accuracy_histogram(
             lower=min_x,
             upper=max_x,
             bin_count=bin_count,
@@ -99,48 +77,3 @@ def column_server(
             error=accuracy,
             cutoff=0,  # TODO
         )
-
-
-def _make_confidence_accuracy_histogram(
-    lower=None, upper=None, bin_count=None, contributions=None, weighted_epsilon=None
-):
-    # Mock data only depends on min and max, so it could be cached,
-    # but I'd guess this is dominated by the DP operations,
-    # so not worth optimizing.
-    row_count = 100
-    df = mock_data({"value": ColumnDef(lower, upper)}, row_count=row_count)
-
-    # TODO: When this is stable, merge it to templates, so we can be
-    # sure that we're using the same code in the preview that we
-    # use in the generated notebook.
-    cut_points = _make_cut_points(lower, upper, bin_count)
-    context = dp.Context.compositor(
-        data=pl.LazyFrame(df).with_columns(
-            # The cut() method returns a Polars categorical type.
-            # Cast to string to get the human-readable label.
-            pl.col("value")
-            .cut(cut_points)
-            .alias("bin")
-            .cast(pl.String),
-        ),
-        privacy_unit=dp.unit_of(
-            contributions=contributions,
-        ),
-        privacy_loss=dp.loss_of(
-            epsilon=weighted_epsilon,
-            delta=1e-7,  # TODO
-        ),
-        split_by_weights=[1],
-        margins={
-            ("bin",): dp.polars.Margin(
-                max_partition_length=row_count,
-                public_info="keys",
-            ),
-        },
-    )
-    query = context.query().group_by("bin").agg(pl.len().dp.noise())
-
-    confidence = 0.95
-    accuracy = query.summarize(alpha=1 - confidence)["accuracy"].item()
-    histogram = query.release().collect().sort("bin")
-    return (confidence, accuracy, histogram)
