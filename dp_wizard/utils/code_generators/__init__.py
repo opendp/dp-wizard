b1f749d28d339ad5f961dad34128f73beb4ed60a
Chuck McCallum <mccallucc@gmail.com>
2024-11-19 16:48:21 -0500

    create a README

--- dp_wizard/utils/code_generators/__init__.py
+++ dp_wizard/utils/code_generators/__init__.py
@@ -1,10 +1,3 @@
-"""
-Strings of ALL CAPS are replaced in these templates.
-Keeping them in a format which can actually be parsed as python
-makes some things easier, but it is also reinventing the wheel.
-We may revisit this.
-"""
-
 from typing import NamedTuple
 from abc import ABC, abstractmethod
 from pathlib import Path
