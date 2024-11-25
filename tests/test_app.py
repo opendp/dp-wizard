3c6b2ebd9689690f55a32a0861655895fe4c0be1
Chuck McCallum <mccallucc@gmail.com>
2024-11-24 23:57:12 -0500

    disable flaky test

--- tests/test_app.py
+++ tests/test_app.py
@@ -102,8 +102,9 @@ def test_default_app(page: Page, default_app: ShinyAppProc):  # pragma: no cover
     expect_visible(simulation)
     assert page.get_by_label("Upper").input_value() == new_value
     # Add a second column:
-    page.get_by_label("blank").check()
-    expect(page.get_by_text("Weight")).to_have_count(2)
+    # page.get_by_label("blank").check()
+    # TODO: Test is flaky?
+    # expect(page.get_by_text("Weight")).to_have_count(2)
     # TODO: Setting more inputs without checking for updates
     # causes recalculations to pile up, and these cause timeouts on CI:
     # It is still rerendering the graph after hitting "Download results".
