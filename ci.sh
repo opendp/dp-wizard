615a016167125372beb042f280262ed9f9a859fc
Chuck McCallum <mccallucc@gmail.com>
2025-03-06 09:42:27 -0500

    make xdist conditional; Remove kernel install: CI should exercise this

--- ci.sh
+++ ci.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
-pytest -vv --failed-first --numprocesses=auto --cov=dp_wizard
+if [ -z ${CI+x} ]; then
+    echo "Run tests in parallel to save developer time. For single process: 'CI=true ./ci.sh'"
+    pytest -vv --failed-first --numprocesses=auto --cov=dp_wizard
+else
+    echo "Run tests in single process to avoid surprises."
+    coverage run -m pytest -vv --failed-first
+    coverage report
+fi
