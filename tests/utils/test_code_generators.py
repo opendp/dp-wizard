b41570139ad135ca70dbef4549660f1f19f49ed9
Chuck McCallum <mccallucc@gmail.com>
2025-03-20 18:07:29 -0400

    checkpoint: test passes, coverage fails

--- tests/utils/test_code_generators.py
+++ tests/utils/test_code_generators.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 import opendp.prelude as dp
 
-from dp_wizard.analyses import histogram, mean
+from dp_wizard.analyses import histogram, mean, median
 from dp_wizard.utils.code_generators import (
     make_column_config_block,
     Template,
@@ -41,6 +41,28 @@ def test_make_column_config_block_for_mean():
     .fill_nan(0)
     .fill_null(0)
     .dp.mean((0, 100))
+)"""
+    )
+
+
+def test_make_column_config_block_for_median():
+    assert (
+        make_column_config_block(
+            name="HW GRADE",
+            analysis_type=median.name,
+            lower_bound=0,
+            upper_bound=100,
+            bin_count=10,
+        ).strip()
+        == """hw_grade_config = (
+    pl.col('HW GRADE')
+    .cast(float)
+    .fill_nan(0)
+    .fill_null(0)
+    .dp.quantile(0.5, make_cut_points(0, 100, bin_count=100))
+    # todo: Get the bin count from the user?
+    # or get nice round numbers?
+    # See: https://github.com/opendp/opendp/issues/1706
 )"""
     )
 
@@ -229,6 +251,13 @@ mean_plan_column = AnalysisPlanColumn(
     bin_count=0,  # Unused
     weight=4,
 )
+median_plan_column = AnalysisPlanColumn(
+    analysis_type=median.name,
+    lower_bound=5,
+    upper_bound=15,
+    bin_count=0,  # Unused
+    weight=4,
+)
 kwargs = {
     "csv_path": fake_csv,
     "contributions": 1,
@@ -238,8 +267,11 @@ plans = [
     AnalysisPlan(groups=groups, columns=columns, **kwargs)
     for groups in [[], ["class year"]]
     for columns in [
+        # Single:
         {"hw-number": histogram_plan_column},
         {"hw-number": mean_plan_column},
+        # {"hw-number": median_plan_column},
+        # Multiple (might be expanded):
         {"hw-number": histogram_plan_column, "grade": mean_plan_column},
     ]
 ]
