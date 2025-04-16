14dbb077b1ef0bbcc513b5e3fb2d247dddd6cbd8
Chuck McCallum <mccallucc@gmail.com>
2025-04-16 14:16:45 -0400

    skip coverage

--- dependencies.py
+++ dependencies.py
@@ -10,14 +10,14 @@ from subprocess import check_call
 from tomlkit import dumps, parse
 
 
-def echo_check_call(cmd):
+def echo_check_call(cmd):  # pragma: no cover
     print(f"Running: {cmd}")
     # Usually avoid "shell=True",
     # but using it here so we can quote the sed expression.
     check_call(cmd, shell=True)
 
 
-def pip_compile_install(file_name):
+def pip_compile_install(file_name):  # pragma: no cover
     echo_check_call(f"pip-compile --rebuild {file_name}")
     txt_file_name = file_name.replace(".in", ".txt")
     echo_check_call(f"pip install -r {txt_file_name}")
@@ -27,13 +27,13 @@ def pip_compile_install(file_name):
     echo_check_call(f"sed -i '' 's:/.*/dp-wizard/:.../dp-wizard/:' {txt_file_name}")
 
 
-def parse_requirements(file_name):
+def parse_requirements(file_name):  # pragma: no cover
     requirements_path = Path(__file__).parent / file_name
     lines = requirements_path.read_text().splitlines()
     return sorted(line for line in lines if line and not line.strip().startswith("#"))
 
 
-def rewrite_pyproject_toml():
+def rewrite_pyproject_toml():  # pragma: no cover
     pyproject_path = Path(__file__).parent / "pyproject.toml"
     pyproject = parse(pyproject_path.read_text())
     # TODO: Can we split it to have one dependency per line?
@@ -47,11 +47,11 @@ def rewrite_pyproject_toml():
     pyproject_path.write_text(dumps(pyproject))
 
 
-def main():
+def main():  # pragma: no cover
     pip_compile_install("requirements-app.in")
     pip_compile_install("requirements-dev.in")
     rewrite_pyproject_toml()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
