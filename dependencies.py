1d5a6c7af604abb25ca637048796b873e89209d2
Chuck McCallum <mccallucc@gmail.com>
2025-04-16 11:43:47 -0400

    update deps with script

--- dependencies.py
+++ dependencies.py
@@ -1,20 +1,25 @@
 from pathlib import Path
-from tomlkit import dumps, parse, table
+from tomlkit import dumps, parse, items
 
 
 def parse_requirements(file_name):
     requirements_path = Path(__file__).parent / file_name
     lines = requirements_path.read_text().splitlines()
-    return [line for line in lines if line and not line.strip().startswith("#")]
+    return sorted(line for line in lines if line and not line.strip().startswith("#"))
 
 
 def rewrite_pyproject_toml():
-    pyproject = parse((Path(__file__).parent / "pyproject.toml").read_text())
-    pyproject["project"]["dependencies"] = parse_requirements("requirements-app.in")
-    pyproject["project"]["optional-dependencies"]["app"] = parse_requirements(
-        "requirements-app.txt"
+    pyproject_path = Path(__file__).parent / "pyproject.toml"
+    pyproject = parse(pyproject_path.read_text())
+    # TODO: multiline format?
+    # https://tomlkit.readthedocs.io/en/latest/api/#tomlkit.items.Array
+    pyproject["project"]["dependencies"] = parse_requirements(
+        "requirements-app.in",
     )
-    print(dumps(pyproject))
+    pyproject["project"]["optional-dependencies"]["app"] = parse_requirements(
+        "requirements-app.txt",
+    )
+    pyproject_path.write_text(dumps(pyproject))
 
 
 def main():
