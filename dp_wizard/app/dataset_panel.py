6cd3abba3d7fbd43d6b124c75c8c1d0d819bb51e
Chuck McCallum <mccallucc@gmail.com>
2025-04-17 13:55:38 -0400

    unused import

--- dp_wizard/app/dataset_panel.py
+++ dp_wizard/app/dataset_panel.py
@@ -7,7 +7,6 @@ from dp_wizard.utils.argparse_helpers import (
     PUBLIC_TEXT,
     PRIVATE_TEXT,
     PUBLIC_PRIVATE_TEXT,
-    CLIInfo,
 )
 from dp_wizard.utils.csv_helper import get_csv_names_mismatch
 from dp_wizard.app.components.outputs import (
