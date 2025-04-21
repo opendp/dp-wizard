ef336230c7905d7dc923ca2203ede802617c8dfe
Chuck McCallum <mccallucc@gmail.com>
2025-04-21 10:41:43 -0400

    Ceilyn and Ellen suggested more context would be useful

--- dp_wizard/app/analysis_panel.py
+++ dp_wizard/app/analysis_panel.py
@@ -57,8 +57,9 @@ def analysis_ui():
                 ui.markdown(
                     """
                     What is your privacy budget for this release?
-                    Values above 1 will add less noise to the data,
-                    but have a greater risk of revealing individual data.
+                    Many factors including the sensitivity of your data,
+                    the frequency of DP releases,
+                    and the regulatory landscape can be considered.
                     Consider how your budget compares to that of
                     <a href="https://registry.oblivious.com/#public-dp"
                        target="_blank">other projects</a>.
