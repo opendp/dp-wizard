acb08219263c7cee8c34a8451d9c02e6ee35eb7e
Chuck McCallum <mccallucc@gmail.com>
2025-05-08 13:26:49 -0400

    Include groups in means plot title

--- dp_wizard/utils/code_generators/analyses/mean/no-tests/_mean_notebook_output.py
+++ dp_wizard/utils/code_generators/analyses/mean/no-tests/_mean_notebook_output.py
@@ -1,6 +1,9 @@
-if groups:
-    title = (
-        f"DP means for COLUMN_NAME, "
-        f"assuming {contributions} contributions per individual"
-    )
-    plot_bars(STATS_NAME, error=0, cutoff=0, title=title)
+title = (
+    f"DP means for COLUMN_NAME, "
+    f"assuming {contributions} contributions per individual"
+)
+
+group_names = GROUP_NAMES
+if group_names:
+    title += f" (grouped by {'/'.join(group_names)})"
+plot_bars(STATS_NAME, error=0, cutoff=0, title=title)
