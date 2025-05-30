934e002657ca0b61f4fc271fdf40b5731d6ead28
Chuck McCallum <mccallucc@gmail.com>
2025-05-29 22:19:18 -0400

    simplify?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,11 @@ set -euo pipefail
 
 echo "Check git..."
 
+git pull
 git diff --exit-code || die "There should be no local modifications."
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 [ "$BRANCH" = "main" ] || die "Current branch should be 'main', not '$BRANCH'."
-
-git pull
 
 echo "Check tests..."
 
@@ -22,9 +21,7 @@ CI='true' scripts/ci.sh --exitfirst || die "Tests should pass"
 
 echo "Push..."
 
-git branch -D cloud-deployment
-git checkout -b cloud-deployment
-git push -f origin cloud-deployment
+git push -f origin main:cloud-deployment
 
 echo "Redeployed!"
 
