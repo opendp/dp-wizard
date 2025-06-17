441768825e73bb6df75b03c2268d7cc66c9d7138
Chuck McCallum <mccallucc@gmail.com>
2025-06-17 10:31:52 -0400

    refactor report gen template for clarity

--- dp_wizard/utils/code_generators/no-tests/_reports.py
+++ dp_wizard/utils/code_generators/no-tests/_reports.py
@@ -1,7 +1,27 @@
-from yaml import dump
 from pathlib import Path
 import csv
 
+def make_report():
+    return {
+        "inputs": {
+            "data": CSV_PATH,
+            "epsilon": EPSILON,
+            "columns": COLUMNS,
+            "contributions": contributions,
+        },
+        "outputs": OUTPUTS,
+    }
+
+def save_txt_report(report):
+    from yaml import dump
+    Path(TXT_REPORT_PATH).write_text(dump(report))
+
+def save_csv_report(report):
+    flat_report = flatten_dict(report)
+    with Path(CSV_REPORT_PATH).open(mode="w", newline="") as handle:
+        writer = csv.writer(handle)
+        for kv_pair in flat_report.items():
+            writer.writerow(kv_pair)
 
 # https://stackoverflow.com/a/6027615/10727889
 def flatten_dict(dictionary, parent_key=""):
@@ -34,21 +54,6 @@ def flatten_dict(dictionary, parent_key=""):
     return dict(items)
 
 
-report = {
-    "inputs": {
-        "data": CSV_PATH,
-        "epsilon": EPSILON,
-        "columns": COLUMNS,
-        "contributions": contributions,
-    },
-    "outputs": OUTPUTS,
-}
-
-print(dump(report))
-Path(TXT_REPORT_PATH).write_text(dump(report))
-
-flat_report = flatten_dict(report)
-with Path(CSV_REPORT_PATH).open(mode="w", newline="") as handle:
-    writer = csv.writer(handle)
-    for kv_pair in flat_report.items():
-        writer.writerow(kv_pair)
+report = make_report()
+save_txt_report(report)
+save_csv_report(report)
