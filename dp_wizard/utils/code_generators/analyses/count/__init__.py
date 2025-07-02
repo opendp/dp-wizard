b3d8bd9c7e7a47733cf7ce4e4bcf8825bb8ab815
Chuck McCallum <mccallucc@gmail.com>
2025-07-02 11:23:10 -0400

    template docs url

--- dp_wizard/utils/code_generators/analyses/count/__init__.py
+++ dp_wizard/utils/code_generators/analyses/count/__init__.py
@@ -1,3 +1,4 @@
+from dp_wizard import opendp_version
 from dp_wizard.utils.code_template import Template
 
 
@@ -61,9 +62,7 @@ def make_column_config_block(column_name, lower_bound, upper_bound, bin_count):
     snake_name = snake_case(column_name)
     return (
         Template("count_expr", __file__)
-        .fill_expressions(
-            EXPR_NAME=f"{snake_name}_expr",
-        )
+        .fill_expressions(EXPR_NAME=f"{snake_name}_expr", OPENDP_VERSION=opendp_version)
         .fill_values(COLUMN_NAME=column_name)
         .finish()
     )
