380133b8b039e7599794410f54a79bdde4ebf4a2
Chuck McCallum <mccallucc@gmail.com>
2025-07-31 09:18:55 -0400

    more types

--- dp_wizard/types.py
+++ dp_wizard/types.py
@@ -1,6 +1,38 @@
 class AnalysisName(str):
+    """
+    A name like "Histogram" or "Mean".
+    """
+
     pass
 
 
 class ColumnName(str):
+    """
+    The exact column header in the CSV.
+    """
+
+    pass
+
+
+class ColumnLabel(str):
+    """
+    The column label displayed in the UI.
+    """
+
+    pass
+
+
+class ColumnId(str):
+    """
+    The opaque string we pass as a module ID.
+    """
+
+    pass
+
+
+class ColumnIdentifier(str):
+    """
+    A human-readable form that is a valid Python identifier.
+    """
+
     pass
--- dp_wizard/utils/csv_helper.py
+++ dp_wizard/utils/csv_helper.py
@@ -1,18 +1,10 @@
-"""
-We'll use the following terms consistently throughout the application:
-- name: This is the exact column header in the CSV.
-- label: This is the string we'll display.
-- id: This is the opaque string we'll pass as a module ID.
-- identifier: This is a form that can be used as a Python identifier.
-"""
-
 import re
 import polars as pl
 from pathlib import Path
-from dp_wizard.types import ColumnName
+from dp_wizard.types import ColumnName, ColumnId, ColumnLabel, ColumnIdentifier
 
 
-def read_csv_names(csv_path: Path):
+def read_csv_names(csv_path: Path) -> list[ColumnName]:
     # Polars is overkill, but it is more robust against
     # variations in encoding than Python stdlib csv.
     # However, it could be slow:
@@ -23,7 +15,9 @@ def read_csv_names(csv_path: Path):
     return [ColumnName(name) for name in lf.collect_schema().names()]
 
 
-def get_csv_names_mismatch(public_csv_path: Path, private_csv_path: Path):
+def get_csv_names_mismatch(
+    public_csv_path: Path, private_csv_path: Path
+) -> tuple[set[ColumnName], set[ColumnName]]:
     public_names = set(read_csv_names(public_csv_path))
     private_names = set(read_csv_names(private_csv_path))
     extra_public = public_names - private_names
@@ -31,22 +25,23 @@ def get_csv_names_mismatch(public_csv_path: Path, private_csv_path: Path):
     return (extra_public, extra_private)
 
 
-def get_csv_row_count(csv_path: Path):
+def get_csv_row_count(csv_path: Path) -> int:
     lf = pl.scan_csv(csv_path)
     return lf.select(pl.len()).collect().item()
 
 
-def id_labels_dict_from_names(names: list[ColumnName]):
+def id_labels_dict_from_names(names: list[ColumnName]) -> dict[ColumnId, ColumnLabel]:
     """
     >>> id_labels_dict_from_names(["abc"])
     {'...': '1: abc'}
     """
     return {
-        name_to_id(name): f"{i+1}: {name or '[blank]'}" for i, name in enumerate(names)
+        name_to_id(name): ColumnLabel(f"{i+1}: {name or '[blank]'}")
+        for i, name in enumerate(names)
     }
 
 
-def id_names_dict_from_names(names: list[ColumnName]):
+def id_names_dict_from_names(names: list[ColumnName]) -> dict[ColumnId, ColumnName]:
     """
     >>> id_names_dict_from_names(["abc"])
     {'...': 'abc'}
@@ -54,19 +49,19 @@ def id_names_dict_from_names(names: list[ColumnName]):
     return {name_to_id(name): name for name in names}
 
 
-def name_to_id(name: ColumnName):
+def name_to_id(name: ColumnName) -> ColumnId:
     """
     >>> import re
     >>> assert re.match(r'^[_0-9]+$', name_to_id('xyz'))
     """
     # Shiny is fussy about module IDs,
     # but we don't need them to be human readable.
-    return str(hash(name)).replace("-", "_")
+    return ColumnId(str(hash(name)).replace("-", "_"))
 
 
-def name_to_identifier(name: ColumnName):
+def name_to_identifier(name: ColumnName) -> ColumnIdentifier:
     """
     >>> name_to_identifier("Does this work?!")
     'does_this_work_'
     """
-    return re.sub(r"\W+", "_", name).lower()
+    return ColumnIdentifier(re.sub(r"\W+", "_", name).lower())
