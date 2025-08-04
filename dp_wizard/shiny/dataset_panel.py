37f6bab6216d8f2efe2632ff7b0acce388cb3e35
Chuck McCallum <mccallucc@gmail.com>
2025-08-04 10:01:32 -0400

    clean up demo help on first tab

--- dp_wizard/shiny/dataset_panel.py
+++ dp_wizard/shiny/dataset_panel.py
@@ -35,9 +35,10 @@ def dataset_ui():
             ui.output_ui("input_contributions_ui"),
             ui.output_ui("contributions_validation_ui"),
             output_code_sample(
-                ["Unit of Privacy", ui.output_ui("python_tooltip_ui")],
+                "Unit of Privacy",
                 "unit_of_privacy_python",
             ),
+            ui.output_ui("python_tooltip_ui"),
         ),
         ui.output_ui("define_analysis_button_ui"),
         value="dataset_panel",
@@ -170,27 +171,30 @@ Choose both **Private CSV** and **Public CSV** {PUBLIC_PRIVATE_TEXT}
         #   and it looks like the file input is unset.
         # - After file upload, the internal copy of the file
         #   is renamed to something like "0.csv".
-        return ui.row(
-            ui.input_file(
-                "private_csv_path",
-                [
-                    "Choose Private CSV ",  # Trailing space looks better.
-                    demo_help(
-                        is_demo,
-                        "For the demo, we'll imagine we have the grades "
-                        "on assignments for a class.",
-                    ),
-                ],
-                accept=[".csv"],
-                placeholder=Path(initial_private_csv_path).name,
+        return [
+            demo_help(
+                is_demo,
+                """
+                For the demo, we've provided the grades
+                on assignments for a school class.
+                You don't need to upload an additional file.
+                """,
             ),
-            ui.input_file(
-                "public_csv_path",
-                "Choose Public CSV",
-                accept=[".csv"],
-                placeholder=Path(initial_public_csv_path).name,
+            ui.row(
+                ui.input_file(
+                    "private_csv_path",
+                    "Choose Private CSV",
+                    accept=[".csv"],
+                    placeholder=Path(initial_private_csv_path).name,
+                ),
+                ui.input_file(
+                    "public_csv_path",
+                    "Choose Public CSV",
+                    accept=[".csv"],
+                    placeholder=Path(initial_public_csv_path).name,
+                ),
             ),
-        )
+        ]
 
     @render.ui
     def csv_column_match_ui():
@@ -266,12 +270,12 @@ Choose both **Private CSV** and **Public CSV** {PUBLIC_PRIVATE_TEXT}
                 """
             ),
             # Without the input label, the tooltip floats way too far the right.
-            demo_tooltip(
+            demo_help(
                 is_demo,
                 f"""
                 For the demo, we assume that each student
                 can occur at most {contributions()} times
-                in the dataset.
+                in the CSV.
                 """,
             ),
             ui.layout_columns(
@@ -315,7 +319,7 @@ Choose both **Private CSV** and **Public CSV** {PUBLIC_PRIVATE_TEXT}
         return demo_help(
             is_demo,
             """
-            Along the way, code samples will demonstrate
+            Along the way, code samples demonstrate
             how the information you provide is used in the
             OpenDP Library, and at the end you can download
             a notebook for the entire calculation.
