5bd4afae0456d51285c8acd1d18b3d96c4589e39
Chuck McCallum <mccallucc@gmail.com>
2025-10-02 14:37:49 -0400

    remove references to google forms

--- dp_wizard/shiny/about_panel.py
+++ dp_wizard/shiny/about_panel.py
@@ -110,26 +110,6 @@ def about_ui():
                         class_="bslib-gap-spacing html-fill-container",
                     ),
                 ),
-                ui.accordion_panel(
-                    "Give Feedback",
-                    ui.div(
-                        HTML(
-                            # Responses to this survey are at:
-                            # https://docs.google.com/forms/d/1l7-RK1R1nRuhHr8pTck1D4RU8Bi6Ehr124bkYvH-96c/edit
-                            """
-                            <iframe
-                                src="https://docs.google.com/forms/d/e/1FAIpQLScaGdKS-vj-RrM7SCV_lAwZmxQ2bOqFrAkyDp4djxTqkTkinA/viewform?embedded=true"
-                                id="feedback-iframe"
-                                width="640"
-                                height="1003"
-                                frameborder="0"
-                                marginheight="0"
-                                marginwidth="0"
-                            >Loading…</iframe>
-                            """
-                        ),
-                    ),
-                ),
                 open=False,
             ),
         ),
