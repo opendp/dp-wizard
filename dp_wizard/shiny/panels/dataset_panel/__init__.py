865c378e966e231e0a5df888822b66507422a498
Chuck McCallum <mccallucc@gmail.com>
2025-10-28 11:48:34 -0400

    pull out csv_or_columns_ui

--- dp_wizard/shiny/panels/dataset_panel/__init__.py
+++ dp_wizard/shiny/panels/dataset_panel/__init__.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from typing import Optional
 
-from dp_wizard_templates.code_template import Template
 from shiny import Inputs, Outputs, Session, reactive, render, ui
 
-from dp_wizard import opendp_version, package_root
 from dp_wizard.shiny.components.icons import (
     data_source_icon,
     product_icon,
@@ -19,12 +17,8 @@ from dp_wizard.shiny.components.outputs import (
     only_for_screenreader,
     tutorial_box,
 )
+from dp_wizard.shiny.panels.dataset_panel import data_source
 from dp_wizard.types import AppState, Product
-from dp_wizard.utils.argparse_helpers import (
-    PRIVATE_TEXT,
-    PUBLIC_PRIVATE_TEXT,
-    PUBLIC_TEXT,
-)
 from dp_wizard.utils.code_generators import make_privacy_unit_block
 from dp_wizard.utils.csv_helper import get_csv_names_mismatch, read_csv_names
 
@@ -215,77 +209,10 @@ def dataset_server(
 
     @render.ui
     def csv_or_columns_ui():
-        if in_cloud:
-            content = [
-                ui.markdown(
-                    """
-                    Provide the names of columns you'll use in your analysis,
-                    one per line, with no extra punctuation.
-                    """
-                ),
-                tutorial_box(
-                    is_tutorial_mode(),
-                    """
-                    When [installed and run
-                    locally](https://pypi.org/project/dp_wizard/),
-                    DP Wizard allows you to specify a private and public CSV,
-                    but for the safety of your data, in the cloud
-                    DP Wizard only accepts column names.
-
-                    If you don't have other ideas, we can imagine
-                    a CSV of student quiz grades: Enter `student_id`,
-                    `quiz_id`, `grade`, and `class_year_str` below,
-                    each on a separate line.
-                    """,
-                    responsive=False,
-                ),
-                ui.input_text_area("column_names", "CSV Column Names", rows=5),
-            ]
-        else:
-            content = [
-                ui.markdown(
-                    f"""
-Choose **Private CSV** {PRIVATE_TEXT}
-
-Choose **Public CSV** {PUBLIC_TEXT}
-
-Choose both **Private CSV** and **Public CSV** {PUBLIC_PRIVATE_TEXT}
-                    """
-                ),
-                ui.output_ui("input_files_ui"),
-                ui.output_ui("csv_column_match_ui"),
-            ]
-
-        content += [
-            code_sample(
-                "Context",
-                Template(
-                    # NOTE: If stats vs. synth is moved to the top of the flow,
-                    # then we can show the appropriate template here.
-                    "stats_context",
-                    package_root / "utils/code_generators/no-tests",
-                )
-                .fill_values(CSV_PATH="sample.csv")
-                .fill_expressions(
-                    MARGINS_LIST="margins",
-                    EXTRA_COLUMNS="extra_columns",
-                    OPENDP_V_VERSION=f"v{opendp_version}",
-                    WEIGHTS="weights",
-                )
-                .fill_code_blocks(
-                    PRIVACY_UNIT_BLOCK="",
-                    PRIVACY_LOSS_BLOCK="",
-                    OPTIONAL_CSV_BLOCK=(
-                        "# More of these slots will be filled in\n"
-                        "# as you move through DP Wizard.\n"
-                    ),
-                )
-                .finish()
-                .strip(),
-            ),
-            ui.output_ui("python_tutorial_ui"),
-        ]
-        return content
+        return data_source.csv_or_columns_ui(
+            in_cloud=in_cloud,
+            is_tutorial_mode=is_tutorial_mode,
+        )
 
     @render.ui
     def input_files_ui():
