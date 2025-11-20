a900351258bc894d477d4567497639cb7c73ac2e
Chuck McCallum <mccallucc@gmail.com>
2025-11-20 10:29:17 -0500

    remove unneeded ".cast(float)"

--- dp_wizard/utils/code_generators/analyses/median/no-tests/_median_expr.py
+++ dp_wizard/utils/code_generators/analyses/median/no-tests/_median_expr.py
@@ -2,8 +2,8 @@
 # https://docs.opendp.org/en/OPENDP_V_VERSION/getting-started/tabular-data/essential-statistics.html#Median
 
 EXPR_NAME = (
-    pl.col(COLUMN_NAME)
-    .cast(float)
-    .dp.quantile(0.5, make_cut_points(LOWER_BOUND, UPPER_BOUND, bin_count=BIN_COUNT))
+    pl.col(COLUMN_NAME).dp.quantile(
+        0.5, make_cut_points(LOWER_BOUND, UPPER_BOUND, bin_count=BIN_COUNT)
+    )
     # Or use "dp.median" which provides 0.5 implicitly.
 )
