74d7c7f7420309843af49de367601f028b2cbd13
Chuck McCallum <mccallucc@gmail.com>
2025-12-15 14:50:01 -0500

    handle output if too large

--- dp_wizard/utils/code_generators/no-tests/_synth_reports.py
+++ dp_wizard/utils/code_generators/no-tests/_synth_reports.py
@@ -16,4 +16,8 @@ report = {
 Path(TXT_REPORT_PATH).write_text(dump(report))
 
 synthetic_data.write_csv(CSV_REPORT_PATH)
-contingency_table_melted.write_csv(CONTINGENCY_TABLE_PATH)
+if isinstance(contingency_table_melted, str):
+    # If too many rows, contingency_table_melted is just an error message:
+    Path(CONTINGENCY_TABLE_PATH).write_text(contingency_table_melted)
+else:
+    contingency_table_melted.write_csv(CONTINGENCY_TABLE_PATH)
