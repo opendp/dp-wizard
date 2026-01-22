b4a4e7ccdca70bc6789bb24e3b59c153513f44b9
Chuck McCallum <mccallucc@gmail.com>
2026-01-22 15:55:48 -0500

    html escape user-provided text

--- dp_wizard/shiny/panels/results_panel/__init__.py
+++ dp_wizard/shiny/panels/results_panel/__init__.py
@@ -1,3 +1,4 @@
+import html
 import re
 from pathlib import Path
 from shutil import make_archive
@@ -372,7 +373,11 @@ def results_server(
             return "raise Exception('qa_mode!')"
         return NotebookGenerator(
             analysis_plan(),
-            input.custom_download_note(),
+            # The custom download note is inserted as a comment,
+            # and jupytext copies it verbatim to a markdown cell,
+            # and nbconvert does not sanitize MD before converting to HTML.
+            # Sanitizing HTML would be more complicated, and potentially lossy.
+            html.escape(input.custom_download_note(), quote=False),
         ).make_py()
 
     @reactive.calc
