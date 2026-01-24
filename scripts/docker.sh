6bc1725a0866558a686de7e473d9f290b8851e9c
Chuck McCallum <mccallucc@gmail.com>
2026-01-23 19:11:41 -0500

    stub docker... but the port does not connect

---
+++ scripts/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker build -t opendp/dp-wizard .
+docker run -p 127.0.0.1:8000:8000 opendp/dp-wizard
+
+# TODO: The image starts successfully,
+# but I'm not able to connect to http://localhost:8000/
+# What am I doing wrong?
