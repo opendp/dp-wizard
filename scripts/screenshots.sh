89f70e8e5132013c2b42ccea2f2d291cd3c1f64a
Chuck McCallum <mccallucc@gmail.com>
2026-03-09 15:02:45 -0400

    One pass is sufficient for screenshots

--- scripts/screenshots.sh
+++ scripts/screenshots.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-SCREENSHOTS=1 pytest -k test_local_app_downloads -vv
+# Test is parameterized: We only need one pass, so download the README.
+SCREENSHOTS=1 pytest -k 'test_local_app_downloads and README' -vv
